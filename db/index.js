aba96b8cb070ca9a94944dfd4a92444a2cd80f7e
Carey Lee <careyl96@gmail.com>
2018-05-02 10:57:32 -0700

    first commit

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const mongoUrl = 'mongodb://admin:notairbnb@ds259109.mlab.com:59109/community-module';
+// const mongoUrl = 'mongodb://admin:notairbnb@ds259109.mlab.com:59109/community-module';
+const mongoUrl = 'mongodb://localhost:27017/community-module';
 
 const db = mongoose.connect(mongoUrl);
 
