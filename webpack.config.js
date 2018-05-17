c7f750a42f3bede46d12be04f761fbd57b44d26e
Carey Lee <careyl96@gmail.com>
2018-05-16 18:27:09 -0700

    webpack bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,39 @@
 const path = require('path');
 
-module.exports = {
-  entry: './client/src/index.jsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'client/dist'),
-  },
-  devtool: '#eval-source-map',
+// module.exports = {
+//   entry: './client/src/index.jsx',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'client/dist'),
+//   },
+//   devtool: '#eval-source-map',
+//   module: {
+//     rules: [
+//       {
+//         test: /.jsx?$/,
+//         loader: 'babel-loader',
+//         exclude: /node_modules/,
+//         query: {
+//           presets: ['es2015', 'react'],
+//         },
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: ['.js', '.jsx'],
+//   },
+// };
+
+const common = {
+  context: path.resolve(__dirname, 'client/src'),
   module: {
     rules: [
       {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
+        test: /\.jsx?$/,
         exclude: /node_modules/,
+        loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react'],
+          presets: ['react', 'es2015']
         },
       },
     ],
@@ -23,3 +42,26 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
 };
+
+const client = {
+  entry: './client.js',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
+  }
+};
+
+const server = {
+  entry: './server.js',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'serverbundle.js',
+    libraryTarget: 'commonjs-module'
+  }
+};
+
+module.exports = [
+  Object.assign({}, common, client),
+  Object.assign({}, common, server)
+];
